aa1b7c8bfa38c101a8dd154e746ca9eb50481f59
Elorvetti <42946687+Elorvetti@users.noreply.github.com>
2019-10-23 01:25:11 +0200

    V0.8.1
    - Bug on edit user

--- admin/wwwroot/js/site.js
+++ admin/wwwroot/js/site.js
@@ -1,119 +1,7 @@
 ﻿"use strict"
 
 var appController = (function(){
-
-    var toogle = function(event){
-        if($(event.data.element).hasClass(event.data.displayClass)){
-            $(event.data.element).removeClass(event.data.displayClass)
-        } else {
-            $(event.data.element).addClass(event.data.displayClass)
-        }
-    }
-
-    var UpdateListUser = function(event){
-       var sectionId;
-
-        if(event !== 'user'){
-            sectionId = $(event.data.sectionName).attr('id');
-        } else {
-            sectionId = event
-        }
-
-        if(sectionId === "user"){
-            fetch('/User/GetAllUsers', { method: 'POST' })
-                .then(function(res){ 
-                   return res.json()
-                    .then(function(data){
-                        for(var i in data){
-                            var element = createListOfUsers(data, i);
-                            $('div.content > ul').append(element)
-                        }
-                   }) 
-                })
-        }
-
-        //Add trigger for edit element and remove element
-        $(document).on('click', '.btn.edit', function(){
-            var id = parseInt($(this).parent().attr('id'));
-            createElementForUpdate(id)
-        });
-        $(document).on('click', '.btn.remove', removeElementToListConfirm)
-    }
-
-    var createListOfUsers = function(obj, i){
-        var element = '<li class="list" id="' + obj[i].id +'">'
-        var element = element + '<p active="' + obj[i].isActive + '">' + obj[i].user + '</p>';
-        var element = element + '<span class="btn btn-circle edit background-color-blue"></span>';
-        var element = element + '<span class="btn btn-circle remove background-color-red"></span>';
-        var element = element + '</li>';
-
-        return element;
-    }
-
-    var createElementForUpdate = function(id){
-        
-        fetch('/User/GetUserById/' + id, { method: 'POST' })
-            .then(function(res) { 
-                return res.json()
-                    .then(function(data){
-
-                        //Create and append overlay
-                        var overlayAndForm = '<div id="overlay"><span class="btn close"></span><form id="' + data.id + '" class="box-shadow border-radius-small text-center background-color-white" autocomplete="off"></form></div>';
-                        $('body').append(overlayAndForm);
-                        
-                        //Create and append input
-                        var element = createElementOfEdit(data);
-                        $('#overlay > form').append(element);
-                    })
-             })
-
-             $(document).on('click', '.update-psw', createElementForUpdatePsw);
-             $(document).on('click', '.btn.close, .btn.return', removeOverlay);
-             $(document).on('click', '#update', updateElement);        
-    }
-
-    var createElementForUpdatePsw = function(){
-        console.log($(this).parent());
-    }
-
-    var updateElement = function(){
-        var $form = $(this).parent().parent();
-        var id = $form.attr('id')
-        var data = $form.serialize()
-
-        $.ajax({
-            method: 'POST',
-            data: data,
-            url: '/User/UpdateUser/' + id,
-            success: function(){
-                $('div#overlay').remove();
-
-                $('ul#user li').remove();
-                UpdateListUser('user');
-
-                successDisplay('Utente aggiornato');
-            }
-
-
-        })
-    }
-
-    var createElementOfEdit = function(obj){
-        var element = '<input name="email" class="name" autocomplete="off" value="'+  obj.user + '" disabled />'
-        if(obj.isActive === true){
-            element =  element + '<input name="active" id="IsActive" type="checkbox" class="is-active btn-switch" checked>'
-        } else {
-            element =  element + '<input name="active" id="IsActive" type="checkbox" class="is-active btn-switch">'
-        }
-        element = element + '<label for="IsActive" data-off="non attivo" data-on="attivo"></label>'
-        element = element + '<p class="update-psw text-underline color-blue-light text-right margin-top-small"> aggiorna password </p>'
-        element = element + '<div class="text-right">'
-        element = element + '<input type="button" id="return" class="btn btn-rounded return text-center color-white box-shadow background-color-red margin-top-small" value="Indietro">'
-        element = element + '<input type="button" id="update" class="btn btn-rounded btn-submit text-center color-white box-shadow background-color-blue-light margin-top-small" value="Salva">'
-        element = element + '</div>';
-
-        return element;
-    }
+    
 
     var removeElementToListConfirm = function(){
         var $li = $(this).parent();
@@ -145,80 +33,210 @@ var appController = (function(){
         //    }
         //})
     }
+     
+    var editElement = function(event){
+        var id = parseInt($(this).parent().attr('id'));
+        var overlay = createOverlay();
 
-    var addElement = function(event){
-        
-        //1. Get section id and form add element
-        var sectionId = $(event.data.container).attr('id');
-        var data = event.data.addElementArray;
-        
-        //2. Add container for add element on section
-        $('body').append(event.data.addContainer);
-        
-        var $form = $(event.data.formAdd)
-       
-        //2. Append element        
-        $form.append(data[sectionId].email);
-        $form.append(data[sectionId].password);
-        $form.append(data[sectionId].confirmPassword);
-        $form.append(data[sectionId].isActive);
-
-        //3. Add save btn to add form
-        $form.append(event.data.btnSave);
-        
-        //4. Validate data
-        validateFormUser($form)
-
-        //4. add trigger of btn
-        $(document).on('click', 'span#close', removeOverlay );
-        $(document).on('click', 'input#save', saveElement);
+        event.data = {
+            id: id,
+            url: '/User/GetUserById/' + id,
+            listContainer: overlay,
+            updateList: false
+        };
+        callbackServer(event);
     }
 
+    var removeElementConfirm = function(){
 
-    var removeOverlay = function(){
-        $('div#overlay').remove();
     }
 
-    var saveElement = function(e){
-        e.preventDefault();
+    var createEdit = function(obj){
+        var element = '<input name="email" class="name" autocomplete="off" value="'+  obj.user + '" disabled />'
+        if(obj.isActive === true){
+            element =  element + '<input name="active" id="IsActive" type="checkbox" class="is-active btn-switch" checked>'
+        } else {
+            element =  element + '<input name="active" id="IsActive" type="checkbox" class="is-active btn-switch">'
+        }
+        element = element + '<label for="IsActive" data-off="non attivo" data-on="attivo"></label>'
+        element = element + '<p class="update-psw text-underline color-blue-light text-right margin-top-small"> aggiorna password </p>'
+        return element;
+    }
 
-        //1. Get form object from DOM
-        var $form = $(this).parent().parent();
+    var createList = function(obj, i){
+        var element = '<li class="list" id="' + obj[i].id +'">'
+        var element = element + '<p active="' + obj[i].isActive + '">' + obj[i].user + '</p>';
+        var element = element + '<span class="btn btn-circle edit background-color-blue"></span>';
+        var element = element + '<span class="btn btn-circle remove background-color-red"></span>';
+        var element = element + '</li>';
 
-        //2. Check if data is valid
-        var state = validateFormUser($form);
+        return element;
+    }
 
-        if (state.invalid.email === false && state.invalid.password === false && state.invalid.confirmPassword === false) {
+    var feedbackEvent = function(success, message){
+        var element;
+        var $overlay = createOverlay();
+        
+        if(success){
+            element = '<span class="box-shadow border-radius-small success text-center color-white">';    
+        } else {
+            element = '<span class="box-shadow border-radius-small error text-center color-white">';    
+        }
+        element = element + message;
+        element = element + '</span>';
+
+        $overlay.after(element);
+        
+    }
+
+    var updateUserList = function(event){
+        
+        //remove element to DOM
+        $('#overlay').remove();
+        $('ul#user li').remove();
+
+        var overlay = createOverlay();
+
+        event.data = {
+            url: '/User/GetAllUsers/',
+            listContainer: overlay,
+            updateList: true
+        };
+
+        if($('ul#user li').length === 0){
+            callbackServer(event)
             
-            //2.1 Serialize Form data
+            //2.1.3 remove overlay
+            feedbackEvent(true, 'Utente aggiunto');
+        } else {
+            feedbackEvent(false, 'ops, si è verificato un errore nell\'aggiornamento utenti, provare a ricicare la pagina');
+        } 
+    }
+
+    var callbackServer = function(event){
+        event.preventDefault();
+
+        var parameter = {
+            post : event.data.post,
+            url: event.data.url,
+        }
+        
+        if(event.data.post){
+            event.preventDefault();
+            
+            //1. Get form from DOM
+            var $form = $(this).parent().parent();
+            var id = $form.attr('id');
+
+            //2. check if we need form validate 
+            if(id === 'add'){
+                var state = validateAddUser($form);
+                if (state.invalid.email === true || state.invalid.password === true || state.invalid.confirmPassword === false) {
+                    if($('form#add > span').length === 0) {
+    
+                        //2.2 show error message    
+                        var errorMessage = '<span class="field-validation-error"> Dati non corretti</span>';
+                        $form.prepend(errorMessage);
+                        return;
+                    }
+                }
+            }
+            
+            //3. Serialize data
             var data = $form.serialize();
-   
+    
             //2.1.1 Pass JSON To server
             $.ajax({
                 method: 'POST',
                 data: data,
+                url: parameter.url,
                 success: function(result){
-                    
-                    //2.1.2 Update list of element
-                    $('ul#user li').remove();
-                    UpdateListUser('user');
-
-                    //2.1.3 remove overlay
-                    $('div#overlay').remove();
-
-                    successDisplay('Utente aggiunto');
+                    event.data.callbackSuccess()
                 }
             })
-        } else if($('form#add > span').length === 0) {
+        } else {
+            fetch(parameter.url,{method: 'POST'})
+                .then(function(res){
+                    return res.json()
+                        .then(function(data){
+                            if(event.data.updateList){
+                                for(var i in data){
+                                    var element = createList(data, i);
+                                    $(event.data.listContainer).append(element);
+                                }
+                            } else {
+                                var $overlay = createOverlay();
+                                var element = createEdit(data);
+                                
+                                //addEditForm($overlay, data, element);
+                            }
+                        })
+                })
 
-            //2.2 Form data not valid show error message    
-            var errorMessage = '<span class="field-validation-error"> Dati non corretti</span>';
-            $form.prepend(errorMessage);
+            //Add event listener on list btn
+            $(document).on('click', '.btn.edit', editElement);
+            $(document).on('click', '.btn.remove', removeElementConfirm);
         }
-
     }
 
-    var validateFormUser = function(form){
+    var addEditForm = function($overlay, data, element){
+        var form = '<form id="' + data.id + '" class="box-shadow border-radius-small text-center background-color-white" autocomplete="off"></form>';
+
+        //append form
+        $overlay.after(form);
+        
+        //get form selector and append data
+        var $form = $('#overlay > form');
+        $form.append(element);
+        
+        var url = '/User/UpdateUser/' + data.id;
+        createBtnForm($form, removeOverlay, {post: true, url: url, callbackSuccess: updateUserList} ,callbackServer);
+    }
+
+    var addUserForm = function(event){
+        console.log('add btn pressed');
+
+        //1. Get section id and form add element
+        var sectionId = $(event.data.sectionId).attr('id');
+        var form = event.data.formData;
+        
+        //2. Create overlay and append form into overlay
+        var $overlay = createOverlay();
+        $overlay.after(event.data.formAddHTML);
+        
+        //2.1 Get form added by id
+        var $form = $(event.data.formAddId)
+       
+        //3. Append element to form
+        
+        //3.1 Append element to set      
+        $form.append(form[sectionId].email);
+        $form.append(form[sectionId].password);
+        $form.append(form[sectionId].confirmPassword);
+        $form.append(form[sectionId].isActive);
+
+        //3.2 append btn 
+        createBtnForm($form, removeOverlay, { post: true, url: '/User/Index', callbackSuccess: updateUserList } ,callbackServer)
+        
+        //4. Add validation to form
+        validateAddUser($form);
+     
+    }
+
+    var createBtnForm = function($form, callbackReturn, callbackSaveParams,callbackSave){
+        var element = '<div class="text-right">';
+        element = element + '<input type="button" id="return" class="btn btn-rounded return text-center color-white box-shadow background-color-red margin-top-small" value="Indietro">';
+        element = element + '<input type="submit" id="save" class="btn btn-rounded btn-submit text-center color-white box-shadow background-color-blue-light margin-top-small" value="Salva">';
+        element = element + '</div>';
+
+        $form.append(element);
+
+        //trigger btn events
+        $(document).on('click', '#return', callbackReturn);
+        $(document).on('click', '#save', callbackSaveParams ,callbackSave);
+    }
+
+    var validateAddUser = function(form){
         
         $.validator.addMethod('password_regex', function(value, element, regex){
             return regex.test(value)
@@ -259,19 +277,39 @@ var appController = (function(){
 
     }
 
-    var successDisplay = function(message){
-        var element = '<div id="overlay"><span class="box-shadow border-radius-small success text-center color-white">';
-        var element = element + message;
-        var element = element + '</span></div>';
+    var createOverlay = function(){
+        var overlay = '';
+        var overlay = '<div id="overlay"><span id="close" class="btn close"></span></div>';
+        
+        //append overlay to DOM
+        $('body').append(overlay);
 
-        $('body').append(element);
-        $(document).on('click', function(){ $('#overlay').remove();})
+        //trigger close click
+        $(document).on('click', '#close', removeOverlay);
+
+        //return selector for append element to overlay
+        var $selector = $('#close');
+
+        return $selector;
+
+    }
+
+    var removeOverlay = function(){
+        $('div#overlay').remove();
+    }
+
+    var toogle = function(event){
+        if($(event.data.element).hasClass(event.data.displayClass)){
+            $(event.data.element).removeClass(event.data.displayClass)
+        } else {
+            $(event.data.element).addClass(event.data.displayClass)
+        }
     }
 
     return{
         toogleClass: toogle,
-        addElement: addElement,
-        UpdateListUser: UpdateListUser,
+        addUserForm: addUserForm,
+        callbackServer: callbackServer,
     }
 
 })();
@@ -286,10 +324,10 @@ var appUI = (function(){
         Content: {
             btnAdd: '.btn.add',
             btnClose: 'span#close',
-            box: 'div.content',
-            addContainerHTML: '<div id="overlay"><span id="close" class="btn close"></span><form id="add" class="box-shadow border-radius-small text-center background-color-white" autocomplete="off"></form></div>',
-            addFormSelector: 'form#add',
-            elementToAppendAdd: {
+            sectionId: 'div.content',
+            formAddHTML: '<form id="add" class="box-shadow border-radius-small text-center background-color-white" autocomplete="off"></form>',
+            formAddId: 'form#add',
+            formData: {
                 addUser: {
                     email: '<input name="email" class="name" placeholder="Email" autocomplete="off" required />',
                     password: '<input name="password" type="password" id="password" class="password" placeholder="Prassword" autocomplete="off" required />',
@@ -297,8 +335,7 @@ var appUI = (function(){
                     isActive: '<input name="active" id="IsActive" type="checkbox" class="is-active btn-switch"><label for="IsActive" data-off="non attivo" data-on="attivo"></label>'
                 }
             },
-            btnSaveHTML: '<div class="text-right"><input type="submit" id="save" class="btn btn-rounded btn-submit text-center color-white box-shadow background-color-blue-light margin-top-small" value="Salva"></div>',
-            contentDisplayElement: 'div.content > ul',
+            listContainer: 'div.content > ul',
         }
 
     }
@@ -320,10 +357,10 @@ var app = (function(UICtrl, appCtrl){
         $(document).on('click', DOMElement.Menu.btnHamburger, {element: DOMElement.Menu.sidebar, displayClass: DOMElement.Menu.displaySidebar}, appCtrl.toogleClass);
 
         //Display list 
-        $(window).on( 'load', { sectionName: DOMElement.Content.contentDisplayElement }, appCtrl.UpdateListUser);
+        $(window).on( 'load', { post: false, url: 'User/GetAllUsers', listContainer: DOMElement.Content.listContainer, updateList: true }, appCtrl.callbackServer);
 
         //Add element to list
-        $(document).on('click', DOMElement.Content.btnAdd, { formAdd: DOMElement.Content.addFormSelector, container: DOMElement.Content.box, addContainer: DOMElement.Content.addContainerHTML, addElementArray: DOMElement.Content.elementToAppendAdd, btnSave: DOMElement.Content.btnSaveHTML }, appCtrl.addElement);
+        $(document).on('click', DOMElement.Content.btnAdd, { formAddId: DOMElement.Content.formAddId, sectionId: DOMElement.Content.sectionId, formAddHTML: DOMElement.Content.formAddHTML, formData: DOMElement.Content.formData }, appCtrl.addUserForm);
         
     }
     
